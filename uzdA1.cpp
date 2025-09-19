a0f593de2797cdced598da539aba15e0fd925f60
bowwowwo <96704375+bowwowwo@users.noreply.github.com>
2025-09-19 15:44:45 +0300

    uzd a (A2) prieks c++

--- uzdA1.cpp
+++ uzdA1.cpp
@@ -9,7 +9,7 @@
 
   A2. Doti naturāli skaitļi m un n. Noskaidrot starpību starp m un n dažādo reizinātāju skaitu (piemēram, m=9, n=6, atbilde ir -1, jo m ir mazāk reizinātāju nekā n).
 
-  Programma izveidota: 09.09.2025.
+  Programma izveidota: 19.09.2025.
 */
 
 #include <iostream>
@@ -17,24 +17,23 @@
 int m;
 int n;
 int ok = 0;
-int count;
 
-int factors(int x);                                    //funkcijas deklarācija jo ir iespējams, ka nestrādā bez tās, arī lai ir vieglāk lasāms kods
+int different_factor_count(int m, int n);                    //funkcijas deklarācija jo ir iespējams, ka nestrādā bez tās
 
 int main()
 {
     do
     {
-        while(1){
+        while (1) {
             std::cout << "Ievadiet skaitli m\n";
-            std::cin >> m;
+            std::cin >> m;                             
 
-            if (m < 1) {                               //nenaturāla skaitļa pārbaude
+            if (m < 1) {                                    //nenaturāla skaitļa pārbaude
                 std::cout << "nav naturāls skaitlis\n";
             }
             else break;
         }
-        
+
         while (1) {
             std::cout << "Ievadiet skaitli n\n";
             std::cin >> n;
@@ -45,7 +44,9 @@ int main()
             else break;
         }
 
-        std::cout << "Skaitļu reizinātāju starpība: " << factors(m) - factors(n) << std::endl;      //tiek uzreiz ievietota funkcija, lai netiktu izmantota papildus atmiņa mainīgo glabāšanai
+
+
+        std::cout << "Skaitļu reizinātāju starpība: " << different_factor_count(m, n) << std::endl; //tiek uzreiz ievietota funkcija, atmiņas ietaupei
 
         std::cout << "Vai turpināt (1) vai beigt (0)?\n";
         std::cin >> ok;
@@ -55,19 +56,45 @@ int main()
     return 0;
 }
 
-int factors(int x)
+int different_factor_count(int m, int n)          //funkcija, kas atgriež argumenta skaitļa reizinātāju skaitu
 {
+   
+    int count = 0;
+    int count_different_m = 0;
+    int count_different_n = 0;
+    int count_same = 0;
 
-    count = 0;
 
-    for (int i = 1; i <= x; i++)
-    {
-        if (x % i == 0) {
-            count++;
+    for (int i = 1; i <= m; i++){                //for loop "sistēma" iegūst vienādo ciparu skaitu m un n
+        if (m % i == 0){
+            for (int j = 1; j <= n; j++){
+                if (n % j == 0) {
+                    if (i == j) {
+                        count_same++;
+                    }
+                }
+            }
         }
     }
 
-    return count;
+    for (int i = 1; i <= m; i++) {              //iegūst reizinātāju skaitu m
+        if (m % i == 0) {
+            count_different_m++;
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {              //iegūst reizinātāju skaitu n
+        if (n % i == 0) {
+            count_different_n++;
+        }
+    }
+
+    count_different_m -= count_same;
+    count_different_n -= count_same;
+
+
+
+    return (count_different_m - count_different_n);
 }
 
 /* testa plāns
@@ -79,7 +106,7 @@ int factors(int x)
 | m=16 n=16    | 0                       | 0                       |
 | m=100 n=10   | 5                       | 5                       |
 | m=-1         | "nav naturāls skaitlis" | "nav naturāls skaitlis" |
+| m=1 n=-1     | "nav naturāls skaitlis" | "nav naturāls skaitlis" |
 +--------------+-------------------------+-------------------------+
-
 
 */
