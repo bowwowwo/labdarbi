2a26a2f2f0054844e90d318e41919b3ea6d85f2a
bowwowwo <96704375+bowwowwo@users.noreply.github.com>
2025-09-19 16:14:35 +0300

    papildus cleanup

--- uzdA1.cpp
+++ uzdA1.cpp
@@ -14,8 +14,8 @@
 
 #include <iostream>
 
-int m;
-int n;
+int m = 0;
+int n = 0;
 int ok = 0;
 
 int different_factor_count(int m, int n);                    //funkcijas deklarācija jo ir iespējams, ka nestrādā bez tās
@@ -24,25 +24,23 @@ int main()
 {
     do
     {
-        while (1) {
+        do {
             std::cout << "Ievadiet skaitli m\n";
-            std::cin >> m;                             
+            std::cin >> m;
 
-            if (m < 1) {                                    //nenaturāla skaitļa pārbaude
+            if (m < 1) {                                    //nenaturāla skaitļa pārbaud
                 std::cout << "nav naturāls skaitlis\n";
             }
-            else break;
-        }
-
-        while (1) {
+        } while (m < 1);
+        
+        do {
             std::cout << "Ievadiet skaitli n\n";
             std::cin >> n;
 
             if (n < 1) {
                 std::cout << "nav naturāls skaitlis\n";
             }
-            else break;
-        }
+        } while (n < 1);
 
 
 
@@ -51,7 +49,7 @@ int main()
         std::cout << "Vai turpināt (1) vai beigt (0)?\n";
         std::cin >> ok;
 
-    } while (ok == 1);
+    } while (ok == 1); 
 
     return 0;
 }
@@ -59,14 +57,14 @@ int main()
 int different_factor_count(int m, int n)          //funkcija, kas atgriež argumenta skaitļa reizinātāju skaitu
 {
    
-    int count = 0;
     int count_different_m = 0;
     int count_different_n = 0;
     int count_same = 0;
 
 
     for (int i = 1; i <= m; i++){                //for loop "sistēma" iegūst vienādo ciparu skaitu m un n
-        if (m % i == 0){
+        if (m % i == 0){                         //iegūst reizinātāju skaitu m
+            count_different_m++;
             for (int j = 1; j <= n; j++){
                 if (n % j == 0) {
                     if (i == j) {
@@ -74,12 +72,6 @@ int different_factor_count(int m, int n)          //funkcija, kas atgriež argum
                     }
                 }
             }
-        }
-    }
-
-    for (int i = 1; i <= m; i++) {              //iegūst reizinātāju skaitu m
-        if (m % i == 0) {
-            count_different_m++;
         }
     }
 
@@ -92,8 +84,6 @@ int different_factor_count(int m, int n)          //funkcija, kas atgriež argum
     count_different_m -= count_same;
     count_different_n -= count_same;
 
-
-
     return (count_different_m - count_different_n);
 }
 
@@ -103,8 +93,9 @@ int different_factor_count(int m, int n)          //funkcija, kas atgriež argum
 | Ievades dati |    Vēlamie rezultāti    |        Rezultāti        |
 +--------------+-------------------------+-------------------------+
 | m=9 n=6      | -1                      | -1                      |
+| m=6 n=9      | 1                       | 1                       |
 | m=16 n=16    | 0                       | 0                       |
-| m=100 n=10   | 5                       | 5                       |
+| m=100 n=10   | 5                       | 5                       | reizinātāju skaits 100 ir 9, reizinātāju skaits 10 ir 4, vienādi reizinātāji ir 4, 5 - 0
 | m=-1         | "nav naturāls skaitlis" | "nav naturāls skaitlis" |
 | m=1 n=-1     | "nav naturāls skaitlis" | "nav naturāls skaitlis" |
 +--------------+-------------------------+-------------------------+
