135ea9620c12e1b22364c23d44864c6ba4493be6
bowwowwo <96704375+bowwowwo@users.noreply.github.com>
2025-09-20 23:10:24 +0300

    python versiajasodkjfasokifjoasfa

--- uzdA.py
+++ uzdA.py
@@ -9,38 +9,56 @@
 
   A2. Doti naturāli skaitļi m un n. Noskaidrot starpību starp m un n dažādo reizinātāju skaitu (piemēram, m=9, n=6, atbilde ir -1, jo m ir mazāk reizinātāju nekā n).
 
-  Programma izveidota: 08.09.2025.
+  Programma izveidota: 19.09.2025.
 """
 m = 0
 n = 0
 ok = 1
 
-def factor_count(x):          #funkcija, kas atgriež argumenta skaitļa reizinātāju skaitu                                # funkcija izmantota, lai kods būtu vieglāk lasāms
-    count = 0
-    x = int(x)    
-    for i in range(x):                                      
-        if x % (i+1) == 0:
-            count += 1
-    return(count)                                           
+def different_factor_count(m ,n):  #funkcija, kas atgriež argumenta skaitļu dažādo reizinātāju skaitu
+    
+    count_different_m = 0
+    count_different_n = 0
+    count_same = 0
 
-while ok == 1:
+    m = int(m) 
+    n = int(n)   
+
+    for i in range(m):         #nested for loop iegūst vienādo reizinātāju skaitu m un n          
+        if m % (i+1) == 0:     #iegūst reizinātāju skaitu m
+          count_different_m += 1
+          for j in range(n):
+            if n % (j+1) == 0:
+              if i == j:       #pārbaude vienādiem reizinātājiem
+                count_same += 1
+
+    for i in range(n):        #iegūst reizinātāju skaitu n
+      if n % (i+1) == 0:
+        count_different_n += 1
+
+    count_different_m -= count_same
+    count_different_n -= count_same
+
+    return(count_different_m - count_different_n)                                           
+
+while ok == 1:                              #programmas darbības sākums
   while True:
     m = int(input("Ievadiet naturālu skaitli m: "))
-    if m < 1:                                            # pārbauda vai ir nenaturāls skaitlis
-      print("nav naturāls skaitlis")
+    if m < 1:
+      print("nav naturāls skaitlis")  # * ne naturāla skaitļa pārbaude
       continue
     else:
       break 
 
   while True:
     n = int(input("Ievadiet naturālu skaitli n: "))
-    if n < 1: 
+    if n < 1:
       print("nav naturāls skaitlis")
       continue
     else:
       break 
         
-  print(f"Skaitļu reizinātāju starpība:  {factor_count(m) - factor_count(n)}")
+  print(f"Skaitļu reizinātāju starpība:  {different_factor_count(m , n)}")
   ok = int(input("Vai turpināt (1) vai beigt (0)?"))
 
 """  testa plāns
@@ -52,11 +70,9 @@ while ok == 1:
 | m=10 n=10    | 0                       | 0                       |
 | m=16 n=4     | 2                       | 2                       |
 | m=-1         | "nav naturāls skaitlis" | "nav naturāls skaitlis" |
+| m=1 n=-1     | "nav naturāls skaitlis" | "nav naturāls skaitlis" |
 +--------------+-------------------------+-------------------------+
 
 """
 
-
   
-
-
