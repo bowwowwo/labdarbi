0b01e9b2ae8d13c21a8539447e2204f4ffab5369
wiyhs <96704375+wiyhs@users.noreply.github.com>
2025-12-01 22:31:11 +0200

    pabeigts d uzd

--- uzdD/uzdD/main.cpp
+++ uzdD/uzdD/main.cpp
@@ -35,16 +35,89 @@ int main()
 
 SetConsoleOutputCP( 65001 );
 
-Nqueue que;
-cout << que.isEmpty() << endl;
-que.enqueue(176);
-cout << que.isEmpty() << endl;
-que.enqueue(176);
-que.enqueue(176);
-que.enqueue(176);
-que.enqueue(176);
-cout << que.Count() << endl;
-cout << que.dequeue() << endl;
-cout << que.Count() << endl;
+int ok;
 
+/*
+Nqueue *queue;
+
+cout << queue.isEmpty() << endl; // ir tukša
+queue.enqueue(1);
+cout << queue.isEmpty() << endl; // nav tukša
+
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue(5);
+queue.enqueue(6);
+
+cout << queue.Count() << endl;
+cout << queue.dequeue() << endl; // izvada 1. elementa vērtību
+cout << queue.Count() << endl;
+*/
+
+Nqueue *queue1;
+queue1 = new Nqueue;
+
+int input;
+int queueInput;
+
+do{
+
+
+
+    cout << "Darbības" << endl << "1 - pievienot rindā skaitli" << endl << "2 - noņemt rindas skaitli" << endl << "3 - rindas lielums" << endl << "4 - uzzināt vai rinda ir tukša" << endl;
+    cin >> input;
+
+    do{
+        switch(input){
+            case 1:
+                cout << "Kādu skaitli pievienot?" << endl;
+                cin >> queueInput;
+                queue1->enqueue(queueInput);
+                break;
+            case 2:
+                cout << "Tika noņemts skaitlis: " << queue1->dequeue() << endl;
+                break;
+            case 3:
+                cout << "Rindas lielums: " << queue1->Count() << endl;
+                break;
+            case 4:
+                cout << ((queue1->isEmpty()) ? "Rinda ir tukša" : "Rinda nav tukša") << endl;
+        }
+    }while(input > 4 && input < 1);
+
+
+
+    cout << " Vai turpināt (1) vai beigt (0)?" << endl;
+    cin >> ok;
+}while(ok == 1);
+
+delete queue1;
 }
+
+
+/*
+
+
+╔═══════════════════════════════╦═══════════════════════════════════╦═══════════════════════════╦═══════════════════════════════════╦═════════════════════════════════════════════════════════╗
+║ Programmas izpildes iterācija ║  Lietotāja izvēlētā darbība (1-4) ║ Papildus lietotāja ievade ║        Gaidāmais rezultāts        ║                        Rezultāts                        ║
+╠═══════════════════════════════╬═══════════════════════════════════╬═══════════════════════════╬═══════════════════════════════════╬═════════════════════════════════════════════════════════╣
+║ 1.                            ║ "3 - rindas lielums"              ║                           ║ 0                                 ║ "Rindas lielums: 0"                                     ║
+║ 2.                            ║ 4 - uzzināt vai rinda ir tukša    ║                           ║ "Rinda ir tukša"                  ║ "Rinda ir tukša"                                        ║
+║ 3.                            ║ "1 - pievienot rindā skaitli"     ║                        90 ║ "Skaitlis pievienots!"            ║ "Skaitlis pievienots!"                                  ║
+║ 4.                            ║ "1 - pievienot rindā skaitli"     ║                        91 ║ "Skaitlis pievienots!"            ║ "Skaitlis pievienots!"                                  ║
+║ 5.                            ║ "3 - rindas lielums"              ║                           ║  2                                ║ Rindas lielums: 2                                       ║
+║ 6.                            ║ "1 - pievienot rindā skaitli"     ║                        92 ║ "Skaitlis pievienots!"            ║ "Skaitlis pievienots!"                                  ║
+║ 7.                            ║ "1 - pievienot rindā skaitli"     ║                        93 ║ "Skaitlis pievienots!"            ║ "Skaitlis pievienots!"                                  ║
+║ 8.                            ║ "1 - pievienot rindā skaitli"     ║                        94 ║ "Skaitlis pievienots!"            ║ "Skaitlis pievienots!"                                  ║
+║ 9.                            ║ "1 - pievienot rindā skaitli"     ║                        94 ║ Rinda ir pilna                    ║ "Rinda ir pilna, noņemt elementu pirms ievietot jaunu!" ║
+║ 10.                           ║ "2 - noņemt rindas skaitli"       ║                           ║ noņemts 1. elements, vērtība = 90 ║ "Noņemts 1. elements Tika noņemts skaitlis: 90"         ║
+║ 11.                           ║ "3 - rindas lielums"              ║                           ║ 4                                 ║ Rindas lielums: 4                                       ║
+║ 12.                           ║ "4 - uzzināt vai rinda ir tukša"  ║                           ║ "Rinda nav tukša"                 ║ "Rinda nav tukša"                                       ║
+║ Izpildes beigas               ║                                   ║                           ║ Nostrādā destruktors              ║ Tiek likvidēts objekts: 0x1007ec0                       ║
+╚═══════════════════════════════╩═══════════════════════════════════╩═══════════════════════════╩═══════════════════════════════════╩═════════════════════════════════════════════════════════╝
+
+
+
+
+*/
--- uzdD/uzdD/uzdD.cpp
+++ uzdD/uzdD/uzdD.cpp
@@ -3,9 +3,45 @@
 
 using namespace std;
 
+/*
+
+Izveidot programmu valodā C++, izmantojot objektorientētās programmēšanas līdzekļus un strukturējot programmu vismaz trīs failos.
+Klases hederi obligāti novietot atsevišķā hedera failā (program.h).
+Visas metodes realizēt ārpus hedera faila – speciālā C++ failā (program.cpp).
+Funkcija main ievietojama vēl citā C++ failā (main.cpp). Klases dati pēc noklusēšanas ir slēpti (private),
+bet metodes atklātas (public). Šīs programmas ietvaros NAV obligāti jānodrošina tāda lietotāja saskarne,
+kāda tika prasīta iepriekšējos trīs uzdevumos (main funkciju drīkst “cieti” iekodēt). Citas prasības sk.
+Laboratorijas darbu noteikumos.
+
+D6. Izveidot klasi "Skaitļu rinda" - Nqueue (queue, pa vienu pusi ieliek, pa otru izņem!),
+kurā glabājas masīvs ar skaitliskām vērtībām (int) garumā pieci un rindas elementu skaits.
+Klasei izveidot šādas metodes: (1) konstruktors, (2) destruktors,
+kurš paziņo par objekta likvidēšanu un likvidēto elementu skaitu (ja likvidēšanas brīdī rinda nav tukša),
+(3) metode "Ielikt" - enqueue, kas pievieno rindai elementu, ja tā nav pilna, (4) metode "Izņemt" - dequeue,
+kas izņem no rindas elementu un atgriež tā vērtību, (5) metode "CikElementu" - count,
+kas atgriež elementu skaitu rindā, (6) metode "IrTukša" - isEmpty, kas noskaidro,
+vai rinda ir tukša. Pirms uzdevuma veikšanas precīzi noskaidrot, ko nozīmē jēdziens rinda (queue) kā datu struktūra.
+
+*/
+
+/*
+Nqueue::Nqueue();
+
+Konstruktors Nqueue() -
+izveidojot objekta Nqueue instanci, piešķir queueCount (rindas pašreizējā garuma mainīgajam) mainīgajam 0
+
+*/
 Nqueue::Nqueue(){
     queueCount = 0;
 }
+
+/*
+Nqueue::~Nqueue();
+
+Destruktors ~Nqueue() -
+tiek izprintēts, ka tiek iznīcināta objekta instance
+
+*/
 
 Nqueue::~Nqueue(){
     if(queueCount != 0){
@@ -15,15 +51,32 @@ Nqueue::~Nqueue(){
     cout << "Tiek likvidēts objekts: " << this << endl;
 }
 
+/*
+void Nqueue::enqueue(int x);
+
+Metode enqueue(x) -
+pievieno objekta Nqueue instances skaitļu masīvam numValue parametrā iegūto skaitli x un palielina rindas pašreizējā garuma mainīgo par 1,
+ja rinda ir pilna, par to paziņo tiek paziņots un iziets no metodes
+*/
+
 void Nqueue::enqueue(int x){  //ievieto jaunu beigās
     if(queueCount == 5){
         cout << "Rinda ir pilna, noņemt elementu pirms ievietot jaunu!" << endl;
         return;
     }
 
+    cout << "Skaitlis " << x << " pievienots!" << endl;
+
     numValue[queueCount] = x;
     queueCount++;
 }
+
+/*
+int Nqueue::dequeue();
+
+Metode dequeue() -
+noņem masīva 1. vērtību pārbīdot visas masīva vērtības uz leju pa 1 un atgriež tā vērtību, kā arī pamazina pašreizējas rindas garuma mainīgo par 1
+*/
 
 int Nqueue::dequeue(){ // nonem pirmo elementu, kā queue datu struktūra
     int removedValue;
@@ -45,9 +98,23 @@ int Nqueue::dequeue(){ // nonem pirmo elementu, kā queue datu struktūra
     return removedValue;
 }
 
+/*
+int Nqueue::Count();
+
+Metode Count() -
+atgriež privāto mainīgo queueCount(masīva pašreizējais lielums)
+*/
+
 int Nqueue::Count(){
     return queueCount;
 }
+
+/*
+bool Nqueue::isEmpty();
+
+Metode isEmpty() -
+atgriež vērtību patiess, ja masīva pašreizējais lielums ir 0
+*/
 
 bool Nqueue::isEmpty(){
     if(queueCount == 0)
